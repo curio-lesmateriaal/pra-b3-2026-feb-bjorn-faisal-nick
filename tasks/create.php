11e768bfc768f4b4eb5e90e3a48f1eae1aeb52ba
Bjorn076x <Bvklaveren@icloud.com>
2026-03-11 12:22:41 +0100

    kleine update

--- tasks/create.php
+++ tasks/create.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulier</title>
 </head>
 <body>
+    <header>
+        <h1>Hoi Allemaal</h1>
+
+    </header>
+    <body>
+        <form action="tasksController.php" method="POST">
+            <label for="naam">Naam:</label>
+            <input type="text" name="naam" id="naam" required>
     
+            <label for="bericht">Bericht:</label>
+            <textarea name="bericht" id="bericht"></textarea>
+    
+            <input type="submit" value="Verzenden">
+        </form>
+        
+    </body>
+    <footer>
+
+    </footer>
 </body>
 </html>

f7720a50a6274f1af6f38e6fd1ed4ac2d41c168a
Bjorn076x <Bvklaveren@icloud.com>
2026-03-11 12:37:27 +0100

    kleine upgrade

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="wrapper">
+                <nav>
+                    <a href="tasks/create.php">create</a>
+                </nav>
+            </div>
+        </header>
+        <body>
+            
+        </body>
+        <footer>
+
+        </footer>
+
+
+    </div>
+
+</body>
+
+</html>

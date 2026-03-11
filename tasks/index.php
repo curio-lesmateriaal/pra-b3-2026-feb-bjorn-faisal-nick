a3dd8728c45751b67680dba8d4d747192d32104d
Bjorn076x <Bvklaveren@icloud.com>
2026-03-11 12:01:37 +0100

    kleine updates

--- tasks/index.php
+++ tasks/index.php
@@ -3,12 +3,24 @@
 
 <head>
     <title></title>
-    <?php require_once 'head.php'; ?>
+    <?php require_once '../head.php'; ?>
 </head>
 
 <body>
-    
     <div class="container">
+        <header>
+            <div class="wrapper">
+                <nav>
+                    <a href="create.php">create</a>
+                </nav>
+            </div>
+        </header>
+        <body>
+            
+        </body>
+        <footer>
+
+        </footer>
 
 
     </div>

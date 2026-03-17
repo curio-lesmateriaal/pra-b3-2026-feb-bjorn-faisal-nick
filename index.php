644c2d4ed55c7bf565cc54f35b70996c20b10057
Nickname-009 <rullensnick@gmail.com>
2026-03-17 11:30:58 +0100

    css link en spelling error fix

--- index.php
+++ index.php
@@ -4,23 +4,29 @@
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
 </head>
 <body>
     <div class="container">
         <header>
             <div class="wrapper">
                 <nav>
+                    <a href="">Personeel</a>
+                    <a href="">Horica</a>
+                    <a href="">Techniek</a>
+                    <a href="">Inkoop</a>
+                    <a href="">Groen</a>
                     <a href="tasks/create.php">create</a>
                 </nav>
             </div>
         </header>
-        <body>
+        <main>
             
-        </body>
+        </main>
         <footer>
 
         </footer>
-
+    </div>
 <div class="container">
 </div>
 

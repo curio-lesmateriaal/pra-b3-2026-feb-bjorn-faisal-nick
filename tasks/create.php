f2891b3305381f825ba7a6fbbadd122eb2dfb231
faysalIl <faisalkhanboy321@gmail.com>
2026-03-18 10:59:31 +0100

    afdeling en hyperlink naar takenlijst

--- tasks/create.php
+++ tasks/create.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks</title>
+    <title>Nieuwe taak</title>
 </head>
 <body>
 
     <h1>Nieuwe taak</h1>
 
-    <form method="POST" action="../controllers/tasksController.php">
-        <input type="hidden" name="action" value="create">
+    <form method="POST" action="../backend/tasksController.php">
+        <input type="hidden" name="action" value="store">
+        <input type="hidden" name="status" value="todo">
 
         <label>Titel:</label><br>
-        <input type="text" name="title"><br><br>
+        <input type="text" name="titel" required><br><br>
 
         <label>Beschrijving:</label><br>
-        <textarea name="description"></textarea><br><br>
+        <textarea name="beschrijving" required></textarea><br><br>
+
+        <label>Afdeling:</label><br>
+        <select name="afdeling" required>
+            <option value="Personeel">Personeel</option>
+            <option value="Horeca">Horeca</option>
+            <option value="Techniek">Techniek</option>
+            <option value="Inkoop">Inkoop</option>
+            <option value="Klantenservice">Klantenservice</option>
+            <option value="Groen">Groen</option>
+        </select><br><br>
 
         <button type="submit">Opslaan</button>
     </form>
 
+    <p><a href="index.php">Terug naar takenlijst</a></p>
+
 </body>
-</html>
 </html>

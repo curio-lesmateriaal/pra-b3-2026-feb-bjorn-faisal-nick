a2ee2a3b4b337d787cebb8e0d093eb8031db2dec
Nickname-009 <rullensnick@gmail.com>
2026-03-18 11:41:38 +0100

    tickets . php test

--- tasks/index.php
+++ tasks/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasks</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
 </head>
 <body>
     <h1>Tasks overzicht</h1>

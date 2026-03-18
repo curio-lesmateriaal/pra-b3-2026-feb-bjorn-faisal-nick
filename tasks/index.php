89817d63d00d4f909dff6376ca95999893021bb9
Bjorn076x <Bvklaveren@icloud.com>
2026-03-18 12:23:42 +0100

    update

--- tasks/index.php
+++ tasks/index.php
@@ -10,13 +10,11 @@
 
 <div class="container">
 
-    <img src="img/logo-big-v4.png" alt="Logo" style="width:120px; margin-bottom:20px;">
+    <img src="../img/logo-big-v4.png" alt="Logo" style="width:120px; margin-bottom:20px;">
 
     <h1>Tasks overzicht</h1>
 
-<<<<<<< HEAD
-    <p><a href="create.php">Nieuwe taak aanmaken</a></p>
-
+    <p><a href="create.php">Nieuwe taak maken</a></p>
     <?php if (isset($_GET['status']) && $_GET['status'] === 'success'): ?>
         <p style="color:green;">Taak succesvol opgeslagen.</p>
     <?php endif; ?>
@@ -28,7 +26,6 @@
     if (file_exists($tasksFile)) {
         if (($handle = fopen($tasksFile, 'r')) !== false) {
             while (($row = fgetcsv($handle)) !== false) {
-                // Verwacht: titel, beschrijving, afdeling, status
                 if (count($row) < 4) {
                     continue;
                 }
@@ -65,15 +62,13 @@
                 <?php foreach ($tasks as $task): ?>
                     <tr>
                         <td><?php echo htmlspecialchars($task['titel']); ?></td>
+                        <td><?php echo htmlspecialchars($task['beschrijving']); ?></td>
                         <td><?php echo htmlspecialchars($task['afdeling']); ?></td>
-                        <td><?php echo htmlspecialchars($task['status']); ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     <?php endif; ?>
-=======
-    <a href="create.php">Nieuwe taak maken</a>
     <a href="edit.php">Taak aanpassen</a>
     <a href="done.php">Bekijk afgeronde taken</a>
     
@@ -82,7 +77,5 @@
     </a>
 
 </div>
-
->>>>>>> 7af2ef28076a9aa0d20944d4a7f66a4a96c8df06
 </body>
 </html>

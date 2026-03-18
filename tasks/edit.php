7c9071b11372609886a4bc85e6a0a6f1d5697a59
Nickname-009 <rullensnick@gmail.com>
2026-03-18 11:25:12 +0100

    temp(?) nav fix

--- tasks/edit.php
+++ tasks/edit.php
@@ -7,7 +7,24 @@
     <link rel="stylesheet" href="../css/main.css" type="text/css">
 </head>
 <body>
-    <?php require_once '../header.php'?>
+    <div class="container">
+        <header>
+            <div class="wrapper">
+                <div class="Topnav">
+                    <nav>
+                        <a href="../index.php">Home</a>
+                        <a href="">Personeel</a>
+                        <a href="">Horica</a>
+                        <a href="">Techniek</a>
+                        <a href="">klanten service</a>
+                        <a href="">Inkoop</a>
+                        <a href="">Groen</a>
+                        <a href="tasks/edit.php">create</a> 
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
     <div class="editDiv">
         <h1>Taak aanpassen</h1>
 

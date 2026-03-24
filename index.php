b064f9d34e52b6944e802f34d3b30db97dc9eb2d
Bjorn076x <Bvklaveren@icloud.com>
2026-03-24 09:54:42 +0100

    e

--- index.php
+++ index.php
@@ -10,18 +10,6 @@
 </head>
 
 <body>
-    <header>
-        <div class="wrapper">
-            <nav>
-                <a href="index.php">home</a>
-                <a href="hobbies.php">hobbies</a>
-                <a href="aboutme.php">about me</a>
-                <a href="contact.php">contact</a>
-                <a href="playlist.php">playlist</a>
-                <a href="projects.php">projects</a>
-            </nav>
-        </div>
-    </header>
     <div class="container">
         <?php require_once 'header.php'; ?>
         <main>

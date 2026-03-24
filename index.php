9e223e6f894da80eb7eef4fab1ae257a3c355403
Nickname-009 <rullensnick@gmail.com>
2026-03-24 09:28:59 +0100

    small clean up

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
         <?php require_once 'header.php'; ?>
         <main>
             <div class="wrapper">
-                <h1>TakenLijstTemp</h1>
                 <div class="imageBig">
                     <img src="img\logo-big-v4.png" alt="logoBig">
                 </div>
